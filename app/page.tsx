12f256ea35a8a24570e14f0217a8fb63c3dad0b3
diogosalvadorb <diogosalvadorb@gmail.com>
2026-04-04 19:17:32 -0300

    feat: implement authentication with better-auth and setup better auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
 
 export default function Home() {
+  const handleGoogleLogin = async () => {
+    const { data, error } = await authClient.signIn.social({
+      provider: "google",
+      callbackURL: "/dashboard",
+    });
+     if (error) {
+      toast.error(error.message);
+      return;
+    }
+  };
+
   return (
-    <div>
-      <h1>Honme</h1>
+    <div >
+      <Button onClick={handleGoogleLogin}>
+        Login with Google
+      </Button>
     </div>
   );
 }
